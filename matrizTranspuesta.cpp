d27512c1cd547ee3aaf16c5ebc5e63f9996172b3
ftorresm <ftorresm@unjbg.edu.pe>
2026-07-08 10:11:57 -0500

    Avance 2: lectura de datos

--- matrizTranspuesta.cpp
+++ matrizTranspuesta.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 int main() {
     int n, m;
-    cin >> n >> m; 
+    cin >> n >> m;
 
-    int matriz[25][25]; 
+    int matriz[25][25];
 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> matriz[i][j];
+        }
+    }
 
     return 0;
 }
