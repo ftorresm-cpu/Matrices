957d6e5d7587a2128c19eb71351828c574132311
ftorresm <ftorresm@unjbg.edu.pe>
2026-07-08 09:58:04 -0500

    Estructura inicial: Declaracion de variables para matriz

---
+++ sumaMatriz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cout << "Numero de filas (M): ";
+    cin >> m;
+    cout << "Numero de columnas (N): ";
+    cin >> n;
+
+    int matriz[100][100];
+    int suma = 0; 
+
+    return 0;
+}
