4b1ade203c36f4e86783e6018196a0e3689eda10
ftorresm <ftorresm@unjbg.edu.pe>
2026-07-08 10:00:20 -0500

    Avance 2: Implementacion de lectura de datos

--- sumaMatriz.cpp
+++ sumaMatriz.cpp
@@ -3,13 +3,45 @@ using namespace std;
 
 int main() {
     int m, n;
-    cout << "Numero de filas (M): ";
+    cout << "Ingrese el numero de filas (M): ";
     cin >> m;
-    cout << "Numero de columnas (N): ";
+    cout << "Ingrese el numero de columnas (N): ";
     cin >> n;
 
     int matriz[100][100];
-    int suma = 0; 
+    int suma = 0;
+
+    // Avance: Lectura de los elementos de la matriz
+    cout << "\nIngrese los elementos de la matriz:\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "Elemento [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+            // Pendiente: Realizar la sumatoria de los elementos
+        }
+    }
+
+    return 0;
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cout << "Ingrese el numero de filas (M): ";
+    cin >> m;
+    cout << "Ingrese el numero de columnas (N): ";
+    cin >> n;
+
+    int matriz[100][100];
+    int suma = 0;
+
+    cout << "\nIngrese los elementos de la matriz:\n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "Elemento [" << i << "][" << j << "]: ";
+            cin >> matriz[i][j];
+        }
+    }
 
     return 0;
 }
