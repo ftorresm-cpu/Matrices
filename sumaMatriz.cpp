f3d71c12c38585e113cc41c72575e0ca6bd859bc
ftorresm <ftorresm@unjbg.edu.pe>
2026-07-08 10:03:13 -0500

    Ejercicio terminado: Sumatoria de elemento de una matriz

--- sumaMatriz.cpp
+++ sumaMatriz.cpp
@@ -11,37 +11,18 @@ int main() {
     int matriz[100][100];
     int suma = 0;
 
-    // Avance: Lectura de los elementos de la matriz
     cout << "\nIngrese los elementos de la matriz:\n";
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            cout << "Elemento [" << i << "][" << j << "]: ";
+            cout << "[" << i << "][" << j << "]: ";
             cin >> matriz[i][j];
-            // Pendiente: Realizar la sumatoria de los elementos
+            
+            suma = suma + matriz[i][j]; 
         }
     }
 
-    return 0;
-#include <iostream>
-using namespace std;
 
-int main() {
-    int m, n;
-    cout << "Ingrese el numero de filas (M): ";
-    cin >> m;
-    cout << "Ingrese el numero de columnas (N): ";
-    cin >> n;
-
-    int matriz[100][100];
-    int suma = 0;
-
-    cout << "\nIngrese los elementos de la matriz:\n";
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << "Elemento [" << i << "][" << j << "]: ";
-            cin >> matriz[i][j];
-        }
-    }
+    cout << suma << "\n";
 
     return 0;
 }
